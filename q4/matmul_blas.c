24d313dac6ae8837c43948aff6a28d9b0cf46428
Alston Liu <liu.als@northeastern.edu>
2025-03-11 03:36:57 -0400

    Separate OpenBLAS & custom matmul implementation for q4.

--- q4/matmul_blas.c
+++ q4/matmul_blas.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <omp.h>
+#include <cblas.h>
 
 #define N 256
 
@@ -9,18 +9,6 @@ double CLOCK() {
     struct timespec t;
     clock_gettime(CLOCK_MONOTONIC, &t);
     return (t.tv_sec * 1000) + (t.tv_nsec*1e-6);
-}
-
-void matmul_omp(float *m1, float *m2, float *m3) {
-    int i,j,k;
-    #pragma omp parallel for simd private(j, k) shared(a, b, c)
-    for(i = 0; i < N; i++) {
-        for(j=0; j < N; j++) {
-            for(k = 0; k < N; k++) {
-                m3[i][j] += m1[i][k] * m2[k][j];
-            }
-        }
-    }
 }
 
 int main() {
@@ -40,14 +28,14 @@ int main() {
         B[i] = i + 1;
     }
 
-    // Perform matrix multiplication using openMP brute-force
+    // Perform matrix multiplication using cblas_sgemm
     start = CLOCK();
-    matmul_omp(A, B, res1);
+    cblas_sgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans, N, N, N, 1.0f, A, N, B, N, 0.0f, res1, N); 
     finish = CLOCK();
     total = finish - start;
-
-    // Print the result matrix 1
-    printf("Mat Mul OpenMP\n");
+    
+    // Print the result matrix 2
+    printf("Mut Mul cblas\n");
     printf("Result 1: %g\n", res1[N-1][N-1]);
     printf("Time Elapsed: %lfms\n", total);
 
